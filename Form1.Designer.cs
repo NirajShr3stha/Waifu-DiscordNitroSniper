4938fc1c14f0726bd948e5142c6020a9382b30f4
XD-WOLF <62610377+nirajxhrestha@users.noreply.github.com>
2020-07-29 09:46:34 +0545

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DiscordNitroSniper
+{
+    partial class frmDiscordNitroSniper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDiscordNitroSniper));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbToken = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblSucc = new System.Windows.Forms.Label();
+            this.lblFail = new System.Windows.Forms.Label();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Discord Token:";
+            // 
+            // tbToken
+            // 
+            this.tbToken.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.tbToken.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbToken.Location = new System.Drawing.Point(98, 34);
+            this.tbToken.Name = "tbToken";
+            this.tbToken.PasswordChar = '*';
+            this.tbToken.Size = new System.Drawing.Size(100, 20);
+            this.tbToken.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Success:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(204, 31);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Failed:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(55, 201);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(249, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Gold;
+            this.lblStatus.Location = new System.Drawing.Point(95, 57);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(31, 13);
+            this.lblStatus.TabIndex = 9;
+            this.lblStatus.Text = "IDLE";
+            // 
+            // lblSucc
+            // 
+            this.lblSucc.AutoSize = true;
+            this.lblSucc.ForeColor = System.Drawing.Color.Lime;
+            this.lblSucc.Location = new System.Drawing.Point(95, 70);
+            this.lblSucc.Name = "lblSucc";
+            this.lblSucc.Size = new System.Drawing.Size(13, 13);
+            this.lblSucc.TabIndex = 10;
+            this.lblSucc.Text = "0";
+            // 
+            // lblFail
+            // 
+            this.lblFail.AutoSize = true;
+            this.lblFail.ForeColor = System.Drawing.Color.Red;
+            this.lblFail.Location = new System.Drawing.Point(95, 83);
+            this.lblFail.Name = "lblFail";
+            this.lblFail.Size = new System.Drawing.Size(13, 13);
+            this.lblFail.TabIndex = 11;
+            this.lblFail.Text = "0";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.rtbLog.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbLog.Location = new System.Drawing.Point(15, 99);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(350, 96);
+            this.rtbLog.TabIndex = 12;
+            this.rtbLog.Text = "";
+            // 
+            // frmDiscordNitroSniper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(114)))), ((int)(((byte)(122)))));
+            this.ClientSize = new System.Drawing.Size(376, 516);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.lblFail);
+            this.Controls.Add(this.lblSucc);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbToken);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmDiscordNitroSniper";
+            this.Text = "Discord Nitro Sniper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.frmDiscordNitroSniper_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbToken;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblSucc;
+        private System.Windows.Forms.Label lblFail;
+        private System.Windows.Forms.RichTextBox rtbLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Discord.Gateway;
+using Discord;
+using System.Diagnostics;
+
+namespace DiscordNitroSniper
+{
+    public partial class frmDiscordNitroSniper : Form
+    {
+        private int succ = 0;
+
+        private int fail = 0;
+
+        private Config config;
+        public static DiscordSocketClient Client { get; set; }
+		private Color borderColor;
+        private int borderWidth = 3;
+		private Color headerColor;
+        private Rectangle headerRect;
+        private Rectangle clientRect;
+        private Rectangle titleRect;
+        private Rectangle miniBoxRect;
+        private Rectangle maxBoxRect;
+        private Rectangle closeBoxRect;
+        private ButtonState miniState;
+        private ButtonState maxState;
+        private ButtonState closeState;
+        private int x = 0;
+        private int y = 0;
+        private readonly Size BUTTON_BOX_SIZE = new Size(15, 15);
+
+        public frmDiscordNitroSniper()
+        {
+			base.Paint += this.frmDiscordNitroSniper_Paint;
+			base.Resize += this.frmDiscordNitroSniper_Resize;
+			base.MouseDown += this.frmDiscordNitroSniper_MouseDown;
+			base.MouseMove += this.frmDiscordNitroSniper_MouseMove;
+			base.MouseUp += this.frmDiscordNitroSniper_MouseUp;
+			base.MouseDoubleClick += this.frmDiscordNitroSniper_MouseDoubleClick;
+			this.borderColor = Color.Black;
+			this.borderWidth = 3;
+			this.headerColor = Color.Black;
+			//this.x = 0;
+			//this.y = 0;
+			//this.BUTTON_BOX_SIZE = new Size(15, 15);
+			InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+			if (File.Exists("Config.json"))
+			{
+				Log("Config.json Located...", Color.Gray);
+				lblStatus.Text = "Connecting...";
+				lblStatus.ForeColor = Color.Gray;
+				Login();
+			}
+			else
+			{
+				Log("Hello User! Please enter your token and click login to start sniping them nitros!", Color.Gray);
+			}
+		}
+
+        private void Login()
+        {
+            config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("Config.json"));
+
+            Client = new DiscordSocketClient();
+            Client.OnLoggedIn += Client_OnLoggedIn;
+            Client.OnMessageReceived += Client_OnMessageReceived;
+            Client.Login(config.Token);
+			tbToken.Text = config.Token;
+			tbToken.Enabled = false;
+			btnLogin.Enabled = false;
+		}
+
+        private void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
+        {
+            Text = "Discord Nitro Sniper - Logged into " + args.User + "!";
+			Log("Logged into " + args.User + "!",Color.Green);
+			lblStatus.Text = "Connected!";
+			lblStatus.ForeColor = Color.Green;
+			
+		}
+        private void Client_OnMessageReceived(DiscordSocketClient client, MessageEventArgs args)
+        {
+            int i = args.Message.Content.IndexOf("discord.gift/");
+
+            if (i != -1)
+            {
+                string gift = args.Message.Content.Substring(i + 13);
+
+                if (gift.Length == 16)
+                {
+                    try
+                    {
+                        Client.RedeemNitroGift(gift, args.Message.ChannelId);
+						Log("Successfully Redeem Nitro", Color.Green);
+						succ++;
+                    }
+                    catch (DiscordHttpException ex)
+                    {
+                        switch (ex.Code)
+                        {
+                            case DiscordError.NitroGiftRedeemed:
+                                Log("Error Nitro gift already redeemed: " + gift,Color.Red);
+								fail++;
+                                break;
+                            case DiscordError.UnknownGiftCode:
+                                Log("Error Invalid nitro gift: " + gift, Color.Red);
+								fail++;
+								break;
+                            default:
+                                Log($"Error Unknown error: {ex.Code} | {ex.ErrorMessage}", Color.Red);
+								fail++;
+								break;
+                        }
+                    }
+					lblFail.Text = fail.ToString();
+					lblSucc.Text = succ.ToString();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText("Config.json", JsonConvert.SerializeObject(new Config { Token = tbToken.Text }));
+            Login();
+        }
+		public void Log(string text, Color col, bool newline = true)
+		{
+
+			AddText("[", false, Color.Black);
+			AddText(DateTime.Now.ToString("HH:mm:ss"), false, Color.Green);
+			AddText("] [", false, Color.Black);
+			AddText("NitroSniper", false, Color.Magenta);
+			AddText("] ", false, Color.Black);
+			AddText(text, newline, col);
+		}
+
+		private void AddText(string text, bool addNewLine, Color color)
+		{
+			rtbLog.SuspendLayout();
+			rtbLog.SelectionColor = color;
+			rtbLog.AppendText(addNewLine
+				? $"{text}{Environment.NewLine}"
+				: text);
+			rtbLog.ScrollToCaret();
+			rtbLog.ResumeLayout();
+		}
+		#region style
+		private void frmDiscordNitroSniper_Paint(object sender, PaintEventArgs e)
+		{
+			using (Brush b = new SolidBrush(this.borderColor))
+			{
+				e.Graphics.FillRectangle(b, this.headerRect);
+			}
+			using (Brush b2 = new SolidBrush(Color.White))
+			{
+				e.Graphics.DrawString(this.Text, this.Font, b2, this.titleRect);
+			}
+			ControlPaint.DrawCaptionButton(e.Graphics, this.miniBoxRect, CaptionButton.Minimize, this.miniState);
+			ControlPaint.DrawCaptionButton(e.Graphics, this.closeBoxRect, CaptionButton.Close, this.closeState);
+			ControlPaint.DrawBorder(e.Graphics, this.clientRect, this.borderColor, this.borderWidth, ButtonBorderStyle.Solid, this.borderColor, this.borderWidth, ButtonBorderStyle.Solid, this.borderColor, this.borderWidth, ButtonBorderStyle.Solid, this.borderColor, this.borderWidth, ButtonBorderStyle.Solid);
+		}
+
+		private void frmDiscordNitroSniper_Resize(object sender, EventArgs e)
+		{
+			this.headerRect = new Rectangle(base.ClientRectangle.Location, new Size(base.ClientRectangle.Width, 25));
+			checked
+			{
+				this.clientRect = new Rectangle(new Point(base.ClientRectangle.Location.X, base.ClientRectangle.Y + 25), (Size)new Point(base.ClientRectangle.Width, base.ClientRectangle.Height - 25));
+				double yOffset = (double)(this.headerRect.Height + this.borderWidth - this.BUTTON_BOX_SIZE.Height) / 2.0;
+				this.titleRect = new Rectangle((int)Math.Round(yOffset), (int)Math.Round(yOffset), (int)Math.Round(unchecked((double)(checked(base.ClientRectangle.Width - 3 * (this.BUTTON_BOX_SIZE.Width + 1))) - yOffset)), this.BUTTON_BOX_SIZE.Height);
+				this.miniBoxRect = new Rectangle(base.ClientRectangle.Width - 2 * (this.BUTTON_BOX_SIZE.Width + 1), (int)Math.Round(yOffset), this.BUTTON_BOX_SIZE.Width, this.BUTTON_BOX_SIZE.Height);
+				this.closeBoxRect = new Rectangle(base.ClientRectangle.Width - 1 * (this.BUTTON_BOX_SIZE.Width + 1), (int)Math.Round(yOffset), this.BUTTON_BOX_SIZE.Width, this.BUTTON_BOX_SIZE.Height);
+				base.Invalidate();
+			}
+		}
+
+		private void frmDiscordNitroSniper_MouseDown(object sender, MouseEventArgs e)
+		{
+			bool flag = this.titleRect.Contains(e.Location);
+			if (flag)
+			{
+				this.x = e.X;
+				this.y = e.Y;
+			}
+			Point mousePos = base.PointToClient(Control.MousePosition);
+			bool flag2 = this.miniBoxRect.Contains(mousePos);
+			if (flag2)
+			{
+				this.miniState = ButtonState.Pushed;
+			}
+			else
+			{
+				bool flag3 = this.maxBoxRect.Contains(mousePos);
+				if (flag3)
+				{
+					this.maxState = ButtonState.Pushed;
+				}
+				else
+				{
+					bool flag4 = this.closeBoxRect.Contains(mousePos);
+					if (flag4)
+					{
+						this.closeState = ButtonState.Pushed;
+					}
+				}
+			}
+		}
+
+		private void frmDiscordNitroSniper_MouseMove(object sender, MouseEventArgs e)
+		{
+			bool flag = this.x != 0 & this.y != 0;
+			if (flag)
+			{
+				base.Location = checked(new Point(base.Left + e.X - this.x, base.Top + e.Y - this.y));
+				this.Refresh();
+			}
+		}
+
+		private void frmDiscordNitroSniper_MouseUp(object sender, MouseEventArgs e)
+		{
+			this.x = 0;
+			this.y = 0;
+			bool flag = this.miniState == ButtonState.Pushed;
+			if (flag)
+			{
+				base.WindowState = FormWindowState.Minimized;
+				this.miniState = ButtonState.Normal;
+			}
+			else
+			{
+				bool flag2 = this.maxState == ButtonState.Pushed;
+				if (flag2)
+				{
+					bool flag3 = base.WindowState == FormWindowState.Normal;
+					if (flag3)
+					{
+						base.WindowState = FormWindowState.Maximized;
+						this.maxState = ButtonState.Checked;
+					}
+					else
+					{
+						base.WindowState = FormWindowState.Normal;
+						this.maxState = ButtonState.Normal;
+					}
+				}
+				else
+				{
+					bool flag4 = this.closeState == ButtonState.Pushed;
+					if (flag4)
+					{
+						base.Close();
+					}
+				}
+			}
+		}
+
+		private void frmDiscordNitroSniper_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			bool flag = this.titleRect.Contains(e.Location);
+			if (flag)
+			{
+				bool flag2 = base.WindowState == FormWindowState.Normal;
+				if (flag2)
+				{
+					base.WindowState = FormWindowState.Maximized;
+					this.maxState = ButtonState.Checked;
+				}
+				else
+				{
+					base.WindowState = FormWindowState.Normal;
+					this.maxState = ButtonState.Normal;
+				}
+			}
+		}
+        #endregion
+    }
+}
